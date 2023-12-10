7dd92136649d469fd4fae001549d4a3a6ab2c7f4
akyrey <stein.dario@gmail.com>
2023-12-10 18:58:44 +0100

    feat(day09): part 1 complete

---
+++ cmd/day09/day09.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/akyrey/aoc-2023/internal"
+)
+
+func scanLine(line string) []int {
+	numbers := make([]int, 0)
+	for _, number := range strings.Split(line, " ") {
+		value, err := strconv.Atoi(number)
+		internal.CheckError(err)
+		numbers = append(numbers, value)
+	}
+	return numbers
+}
+
+func findNextValue(sequence []int) int {
+	if internal.Every(sequence, func(current int) bool {
+		return current == 0
+	}) {
+		return 0
+	}
+
+	newSequence := make([]int, len(sequence)-1)
+	for i := 0; i < len(sequence)-1; i++ {
+		newSequence[i] = sequence[i+1] - sequence[i]
+	}
+
+	return newSequence[len(newSequence)-1] + findNextValue(newSequence)
+}
+
+func main() {
+	f, err := internal.GetFileToReadFrom(9, false)
+	internal.CheckError(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	lines := make([][]int, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, scanLine(line))
+	}
+
+	sum := 0
+	for _, sequence := range lines {
+		sum += sequence[len(sequence)-1] + findNextValue(sequence)
+	}
+
+	fmt.Println(sum)
+}
