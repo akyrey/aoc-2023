2199d9c5ef6288a29bdefe1dc008598f8db1817c
akyrey <stein.dario@gmail.com>
2024-12-03 09:39:18 +0100

    feat(day03): part 2 complete

--- cmd/year2024/day03/day03.go
+++ cmd/year2024/day03/day03.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"regexp"
+	"slices"
 	"strconv"
 
 	"github.com/akyrey/aoc/internal"
@@ -15,14 +16,24 @@ func main() {
 	defer f.Close()
 
 	total := 0
+	reEnable := regexp.MustCompile(`do()`)
+	reDisable := regexp.MustCompile(`don't()`)
 	re := regexp.MustCompile(`mul\(\d+,\d+\)`)
 	reNumbers := regexp.MustCompile(`\d+`)
 	scanner := bufio.NewScanner(f)
+	enabledFromPreviouseLine := true
 	for scanner.Scan() {
 		line := scanner.Text()
-		multiplies := re.FindAllString(line, -1)
-		for _, m := range multiplies {
-			numbers := reNumbers.FindAllString(m, 2)
+		multipliesIndexes := re.FindAllStringIndex(line, -1)
+		enable := reEnable.FindAllStringIndex(line, -1)
+		disable := reDisable.FindAllStringIndex(line, -1)
+		for _, m := range multipliesIndexes {
+			if !isEnabled(enable, disable, m[0], enabledFromPreviouseLine) {
+				enabledFromPreviouseLine = false
+				continue
+			}
+			enabledFromPreviouseLine = true
+			numbers := reNumbers.FindAllString(line[m[0]:m[1]], 2)
 			if len(numbers) != 2 {
 				panic("Invalid input")
 			}
@@ -32,7 +43,41 @@ func main() {
 			internal.CheckError(err)
 			total += one * two
 		}
+		last := multipliesIndexes[len(multipliesIndexes)-1]
+		enabledFromPreviouseLine = enableDisableNextLine(enable, disable, last[1], len(line), enabledFromPreviouseLine)
 	}
 
 	fmt.Printf("%d\n", total)
+}
+
+func isEnabled(enable [][]int, disable [][]int, index int, enabledFromPreviouseLine bool) bool {
+	for i := index; i >= 0; i-- {
+		if slices.ContainsFunc(enable, func(e []int) bool {
+			return e[1] == i
+		}) {
+			return true
+		}
+		if slices.ContainsFunc(disable, func(e []int) bool {
+			return e[1] == i
+		}) {
+			return false
+		}
+	}
+	return enabledFromPreviouseLine
+}
+
+func enableDisableNextLine(enable [][]int, disable [][]int, index int, end int, enabledFromPreviouseLine bool) bool {
+	for i := index; i < end; i++ {
+		if slices.ContainsFunc(enable, func(e []int) bool {
+			return e[0] == i
+		}) {
+			return true
+		}
+		if slices.ContainsFunc(disable, func(e []int) bool {
+			return e[0] == i
+		}) {
+			return false
+		}
+	}
+	return enabledFromPreviouseLine
 }
